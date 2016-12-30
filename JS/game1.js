b7d7610cff13bb5f1ed344d2ca720494c795e205
Weichao Zhao <zhao425772604@gmail.com>
2016-12-29 21:38:40 -0500

    timer function

--- JS/game1.js
+++ JS/game1.js
@@ -1,11 +1,33 @@
-function startToPause() {
+function startGame() {
     "use strict";
     var button = document.getElementById("startButton");
     if (button.value === "Start") {
         button.value = "Pause";
+        timeCount();
     } else {
         button.value = "Start";
     }
    
         
+}
+
+function timeCount() {
+    "use strict";
+    var hou = 0, sec = 60;
+    setInterval(function () {
+
+        document.getElementById("timer").innerHTML = hou + " : " + sec;
+        sec = sec - 1;
+        if (sec === 0) {
+            alert("times out");
+            stopTime();
+        }
+        
+    }, 1000);
+    
+}
+
+function stopTime() {
+    "use strict";
+    document.getElementById("timer").innerHTML = "0:00";
 }
