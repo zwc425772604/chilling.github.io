11545f4d3b1eb8ef586150c8ec777c73b177a634
Weichao Zhao <zhao425772604@gmail.com>
2017-02-07 21:01:20 -0500

    adding jquery

--- js/webgame.js
+++ js/webgame.js
@@ -1,6 +1,31 @@
+$(document).ready(function () {
+  //your code here
+    //$('.game1').hide();
+   // $('.game2').hide();
+    $('.container').hide();
+    $('#game1Button').click(function(){
+        $('.container').show();
+        $('.game1').show();
+        $('.game2').hide();
+    });
+    
+    $('#game2Button').click(function(){
+         $('.container').show();
+        $('.game1').hide();
+        $('.game2').show();
+       
+    });
+    
+    
+});
+
+
 function changeNum(){
+   
+    $('.game1').show();
    var time = 3;
-            document.getElementById("remaining").innerHTML = time + "s";
+            
+            document.getElementById("rollingNums").innerHTML = time + "s";
             var x = setInterval(function() {
                time--;
                if (time == 0) {
@@ -10,11 +35,13 @@ function changeNum(){
                }
 
                //document.getElementById('remaining').stop();
-                document.getElementById("remaining").innerHTML = time + "s";
+                document.getElementById("rollingNums").innerHTML = time + "s";
                 if (time == 0){
-                    document.getElementById("remaining").innerHTML = "";
+                    document.getElementById("rollingNums").innerHTML = "";
+                    $('.rollingMarquee').fadeOut();
+                    time = 3;
                 }
             }, 2000);
 
-            
+        
 }
